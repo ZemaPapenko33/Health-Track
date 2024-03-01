8389eab069c829f34f81a3c85cd21c516295db69
Zahar Papenko <zakharpapenko@gmail.com>
2024-03-01 17:08:03 +0300

    fix: delete button in home page

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,25 +1,9 @@
 import { useDefaultRedirect } from '../hooks/use-default-redirect.hook'
-import { Button } from '@mui/material'
-import { auth } from '../firebase/firebaseConfig'
-import { signOut } from 'firebase/auth'
 
 const HomePage = (): JSX.Element => {
   useDefaultRedirect()
 
-  return (
-    <div>
-      Home page
-      <Button
-        onClick={() => {
-          signOut(auth).then(() => {
-            localStorage.clear()
-          })
-        }}
-      >
-        LOGOUT
-      </Button>
-    </div>
-  )
+  return <div></div>
 }
 
 export default HomePage
