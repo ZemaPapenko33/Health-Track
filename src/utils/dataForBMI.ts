9f375b15c5969ca14cc1f0e63abf8a2f987ea4d9
Zahar Papenko <zakharpapenko@gmail.com>
2024-03-28 12:32:02 +0300

    fix: magical numbers in function for BMI

--- src/utils/dataForBMI.ts
+++ src/utils/dataForBMI.ts
@@ -1,6 +1,18 @@
 import { t } from 'i18next'
 import { TBMIData } from '../Types/ComponentTypes'
 import { theme } from '../theme'
+import {
+  MAX_BMI,
+  NORMAL_WEIGHT_UPPER_THRESHOLD,
+  OBESE_CLASS1_LOWER_THRESHOLD,
+  OBESE_CLASS1_UPPER_THRESHOLD,
+  OBESE_CLASS2_LOWER_THRESHOLD,
+  OBESE_CLASS2_UPPER_THRESHOLD,
+  OBESE_CLASS3_LOWER_THRESHOLD,
+  OBESE_CLASS3_UPPER_THRESHOLD,
+  OVERWEIGHT_LOWER_THRESHOLD,
+  UNDERWEIGHT_THRESHOLD
+} from '../shared/consts/BMI'
 
 export const dataForBMI = (BMI: number): TBMIData => {
   const BMIData: TBMIData = {
@@ -9,31 +21,31 @@ export const dataForBMI = (BMI: number): TBMIData => {
   }
 
   switch (true) {
-    case BMI < 16:
+    case BMI < UNDERWEIGHT_THRESHOLD:
       BMIData.message = t('t-BMI-less-16')
       BMIData.color = theme.palette.lightBlue.main
       break
-    case BMI >= 16 && BMI <= 18.5:
+    case BMI >= UNDERWEIGHT_THRESHOLD && BMI <= NORMAL_WEIGHT_UPPER_THRESHOLD:
       BMIData.message = t('t-BMI-16-18.5')
       BMIData.color = theme.palette.lightBlue.main
       break
-    case BMI >= 18.6 && BMI <= 25:
+    case BMI >= OVERWEIGHT_LOWER_THRESHOLD && BMI <= OVERWEIGHT_LOWER_THRESHOLD:
       BMIData.message = t('t-BMI-18.6-25')
       BMIData.color = theme.palette.primary.main
       break
-    case BMI >= 25.1 && BMI <= 30:
+    case BMI >= OBESE_CLASS1_LOWER_THRESHOLD && BMI <= OBESE_CLASS1_UPPER_THRESHOLD:
       BMIData.message = t('t-BMI-25.1-30')
       BMIData.color = theme.palette.softYellow.main
       break
-    case BMI >= 30.1 && BMI <= 35:
+    case BMI >= OBESE_CLASS2_LOWER_THRESHOLD && BMI <= OBESE_CLASS2_UPPER_THRESHOLD:
       BMIData.message = t('t-BMI-30.1-35')
       BMIData.color = theme.palette.softRed.main
       break
-    case BMI >= 35.1 && BMI <= 40:
+    case BMI >= OBESE_CLASS3_LOWER_THRESHOLD && BMI <= OBESE_CLASS3_UPPER_THRESHOLD:
       BMIData.message = t('t-BMI-35.1-40')
       BMIData.color = theme.palette.softRed.main
       break
-    case BMI > 40.1:
+    case BMI > MAX_BMI:
       BMIData.message = t('t-BMI-more-40.1')
       BMIData.color = theme.palette.softRed.main
       break
