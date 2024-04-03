9ebb76f0733136122832d5f0540d8468f8b74e9a
Zahar Papenko <zakharpapenko@gmail.com>
2024-04-03 15:22:05 +0300

    feat: add reducers for update and delete dateUser slice

--- src/store/slices/dataUserSlice.ts
+++ src/store/slices/dataUserSlice.ts
@@ -27,23 +27,75 @@ const dataUserSlice = createSlice({
     addUserFood: (state, action: PayloadAction<TFood>) => {
       state.userFood.push(action.payload)
     },
+    removeUserFood: (state, action: PayloadAction<number>) => {
+      state.userFood.splice(action.payload, 1)
+    },
+    updateUserFood: (state, action: PayloadAction<{ index: number; newValue: TFood }>) => {
+      const { index, newValue } = action.payload
+      state.userFood[index] = newValue
+    },
     addUserWater: (state, action: PayloadAction<TWater>) => {
       state.userWater.push(action.payload)
+    },
+    removeUserWater: (state, action: PayloadAction<number>) => {
+      state.userWater.splice(action.payload, 1)
+    },
+    updateUserWater: (state, action: PayloadAction<{ index: number; newValue: TWater }>) => {
+      const { index, newValue } = action.payload
+      state.userWater[index] = newValue
     },
     addUserFitness: (state, action: PayloadAction<TFitness>) => {
       state.userFitness.push(action.payload)
     },
+    removeUserFitness: (state, action: PayloadAction<number>) => {
+      state.userFitness.splice(action.payload, 1)
+    },
+    updateUserFitness: (state, action: PayloadAction<{ index: number; newValue: TFitness }>) => {
+      const { index, newValue } = action.payload
+      state.userFitness[index] = newValue
+    },
     addUserMood: (state, action: PayloadAction<TMood>) => {
       state.userMood.push(action.payload)
+    },
+    removeUserMood: (state, action: PayloadAction<number>) => {
+      state.userMood.splice(action.payload, 1)
+    },
+    updateUserMood: (state, action: PayloadAction<{ index: number; newValue: TMood }>) => {
+      const { index, newValue } = action.payload
+      state.userMood[index] = newValue
     },
     addUserSleeps: (state, action: PayloadAction<TSleeps>) => {
       state.userSleeps.push(action.payload)
     },
+    removeUserSleeps: (state, action: PayloadAction<number>) => {
+      state.userSleeps.splice(action.payload, 1)
+    },
+    updateUserSleeps: (state, action: PayloadAction<{ index: number; newValue: TSleeps }>) => {
+      const { index, newValue } = action.payload
+      state.userSleeps[index] = newValue
+    },
     addUserMeditation: (state, action: PayloadAction<TMeditation>) => {
       state.userMeditation.push(action.payload)
     },
+    removeUserMeditation: (state, action: PayloadAction<number>) => {
+      state.userMeditation.splice(action.payload, 1)
+    },
+    updateUserMeditation: (
+      state,
+      action: PayloadAction<{ index: number; newValue: TMeditation }>
+    ) => {
+      const { index, newValue } = action.payload
+      state.userMeditation[index] = newValue
+    },
     addUserHobby: (state, action: PayloadAction<THobby>) => {
       state.userHobby.push(action.payload)
+    },
+    removeUserHobby: (state, action: PayloadAction<number>) => {
+      state.userHobby.splice(action.payload, 1)
+    },
+    updateUserHobby: (state, action: PayloadAction<{ index: number; newValue: THobby }>) => {
+      const { index, newValue } = action.payload
+      state.userHobby[index] = newValue
     },
     resetAll: (state) => {
       state.userFood = []
@@ -65,6 +117,20 @@ export const {
   addUserSleeps,
   addUserMeditation,
   addUserHobby,
+  removeUserFood,
+  removeUserWater,
+  removeUserFitness,
+  removeUserMood,
+  removeUserSleeps,
+  removeUserMeditation,
+  removeUserHobby,
+  updateUserFood,
+  updateUserWater,
+  updateUserFitness,
+  updateUserMood,
+  updateUserSleeps,
+  updateUserMeditation,
+  updateUserHobby,
   resetAll
 } = dataUserSlice.actions
 export default dataUserSlice.reducer
